c825452fe06c10cfbacd83c831a4ebc6631e16e3
Solar <abe@solarcosmic.net>
2025-07-31 08:28:57 +1200

    Update Inputs

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -5,32 +5,35 @@ class_name Intro
 @export var grid_container: GridContainer
 @export var main_label: Label
 @export var letter_scene: PackedScene
+@export var wait_multiplier: float
 var type_speed: float = 15
 var type_time: float
 var is_in_grid = false
 
+func wait(amount):
+	await get_tree().create_timer(amount * wait_multiplier).timeout
+
 func _ready():
-	prepare_grid()
 	$CanvasLayer/Label2.modulate.a = 0
-	$CanvasLayer/GridContainer.modulate.a = 1
 	main_label.text = "An SC Project"
-	await get_tree().create_timer(2.0).timeout
+	await wait(2.0)
 	create_tween().tween_property(main_label, "modulate:a", 0, 1).set_trans(Tween.TRANS_SINE)
-	await get_tree().create_timer(2.0).timeout
+	await wait(2.0)
 	main_label.text = ""
 	main_label.modulate.a = 1
-	await get_tree().create_timer(2.0).timeout
+	await wait(2.0)
 	$CanvasLayer/AudioStreamPlayer.play()
 	display("You don't know who I am.")
-	await get_tree().create_timer(5.0).timeout
+	await wait(5.0)
 	display("... neither do I know who you are.")
-	await get_tree().create_timer(6.0).timeout
+	await wait(6.0)
 	display("Let us create someone new.")
-	await get_tree().create_timer(5.0).timeout
+	await wait(5.0)
 	display("Who do you want to be?")
-	await get_tree().create_timer(2.0).timeout
+	await wait(2.0)
 	create_tween().tween_property(main_label, "position", Vector2($CanvasLayer/Label.position.x, $CanvasLayer/Label.position.y - 65), 1).set_trans(Tween.TRANS_SINE)
-	await get_tree().create_timer(0.5).timeout
+	prepare_grid()
+	await wait(0.5)
 	create_tween().tween_property($CanvasLayer/Label2, "modulate:a", 0.5, 1).set_trans(Tween.TRANS_SINE)
 	create_tween().tween_property($CanvasLayer/GridContainer, "modulate:a", 1, 1).set_trans(Tween.TRANS_SINE)
 
@@ -60,6 +63,7 @@ func add_letter_to_grid(i, name):
 	else:
 		node_ins.modulate = Color(1,1,1)
 
+var cur_index = 0
 func prepare_grid():
 	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	for i in alphabet:
@@ -67,5 +71,9 @@ func prepare_grid():
 	add_letter_to_grid("←", "backspace")
 	is_in_grid = true
 	#grid_container.get_child(0).label_settings.font_color = Color(1, 0, 0)
+	if Input.is_action_just_released("right_arrow"): # doesn't work
+		cur_index += 1
+		print(grid_container.get_child(cur_index))
+		grid_container.get_child(cur_index).modulate = Color(1,0,0)
 	for label in grid_container.get_children():
 		print(label.name)
