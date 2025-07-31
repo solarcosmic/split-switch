eb24ca7ea466b3f8e171bcfe500e541edf8108d3
Solar <abe@solarcosmic.net>
2025-07-31 15:34:58 +1200

    Keypad for Intro

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -6,6 +6,7 @@ class_name Intro
 @export var main_label: Label
 @export var letter_scene: PackedScene
 @export var wait_multiplier: float
+@export var name_label: Label
 var type_speed: float = 15
 var type_time: float
 var is_in_grid = false
@@ -15,6 +16,7 @@ func wait(amount):
 
 func _ready():
 	$CanvasLayer/Label2.modulate.a = 0
+	grid_container.modulate.a = 0
 	main_label.text = "An SC Project"
 	await wait(2.0)
 	create_tween().tween_property(main_label, "modulate:a", 0, 1).set_trans(Tween.TRANS_SINE)
@@ -25,13 +27,13 @@ func _ready():
 	$CanvasLayer/AudioStreamPlayer.play()
 	display("You don't know who I am.")
 	await wait(5.0)
-	display("... neither do I know who you are.")
-	await wait(6.0)
-	display("Let us create someone new.")
-	await wait(5.0)
-	display("Who do you want to be?")
+	display("... I know who you are.")
+	await wait(4.0)
+	display("Let us create another.")
+	await wait(4.0)
+	display("What do you want to name it?")
 	await wait(2.0)
-	create_tween().tween_property(main_label, "position", Vector2($CanvasLayer/Label.position.x, $CanvasLayer/Label.position.y - 65), 1).set_trans(Tween.TRANS_SINE)
+	create_tween().tween_property(main_label, "position", Vector2($CanvasLayer/Label.position.x, $CanvasLayer/Label.position.y - 90), 1).set_trans(Tween.TRANS_SINE)
 	prepare_grid()
 	await wait(0.5)
 	create_tween().tween_property($CanvasLayer/Label2, "modulate:a", 0.5, 1).set_trans(Tween.TRANS_SINE)
@@ -71,9 +73,29 @@ func prepare_grid():
 	add_letter_to_grid("←", "backspace")
 	is_in_grid = true
 	#grid_container.get_child(0).label_settings.font_color = Color(1, 0, 0)
-	if Input.is_action_just_released("right_arrow"): # doesn't work
-		cur_index += 1
-		print(grid_container.get_child(cur_index))
-		grid_container.get_child(cur_index).modulate = Color(1,0,0)
 	for label in grid_container.get_children():
 		print(label.name)
+
+func lsprocess():
+	for label in grid_container.get_children():
+		label.modulate = Color(1,1,1)
+	print(grid_container.get_child(cur_index))
+	grid_container.get_child(cur_index).modulate = Color(1,0,0)
+
+func _process(delta):
+	if is_in_grid:
+		if Input.is_action_just_released("right_arrow"):
+			cur_index += 1
+			print(cur_index)
+			lsprocess()
+		if Input.is_action_just_released("left_arrow"):
+			cur_index -= 1
+			print(cur_index)
+			lsprocess()
+		if Input.is_action_just_released("enter"):
+			var character = grid_container.get_child(cur_index).text
+			if character == "←":
+				name_label.text.erase(-1)
+			else:
+				name_label.text += character
+			
